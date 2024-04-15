21f622a6a6b0c5c7804e7466f4843d0559a4991b
Ishwar Mishra <ikmishra2002@gmail.com>
2024-04-15 15:00:59 +0530

    added roc curve

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-from flask import Flask,render_template,url_for,request,jsonify
+from flask import Flask,render_template,request
 from flask_cors import cross_origin
-import pandas as pd
-import numpy as np
-import datetime
 import pickle
-
-
 
 app = Flask(__name__, template_folder="template")
 model = pickle.load(open("./machine learning/models/XGBoost/xgb.pkl", "rb"))
@@ -22,49 +17,27 @@ def home():
 @cross_origin()
 def predict():
 	if request.method == "POST":
-		# DATE
 		month = float(request.form['date'])
-		# MinTemp
 		minTemp = float(request.form['mintemp'])
-		# MaxTemp
 		maxTemp = float(request.form['maxtemp'])
-		# Rainfall
 		rainfall = float(request.form['rainfall'])
-		# Evaporation
 		evaporation = float(request.form['evaporation'])
-		# Sunshine
 		sunshine = float(request.form['sunshine'])
-		# Wind Gust Speed
 		windGustSpeed = float(request.form['windgustspeed'])
-		# Wind Speed 9am
 		windSpeed9am = float(request.form['windspeed9am'])
-		# Wind Speed 3pm
 		windSpeed3pm = float(request.form['windspeed3pm'])
-		# Humidity 9am
 		humidity9am = float(request.form['humidity9am'])
-		# Humidity 3pm
 		humidity3pm = float(request.form['humidity3pm'])
-		# Pressure 9am
 		pressure9am = float(request.form['pressure9am'])
-		# Pressure 3pm
 		pressure3pm = float(request.form['pressure3pm'])
-		# Temperature 9am
 		temp9am = float(request.form['temp9am'])
-		# Temperature 3pm
 		temp3pm = float(request.form['temp3pm'])
-		# Cloud 9am
 		cloud9am = float(request.form['cloud9am'])
-		# Cloud 3pm
 		cloud3pm = float(request.form['cloud3pm'])
-		# Cloud 3pm
 		location = float(request.form['location'])
-		# Wind Dir 9am
 		winddDir9am = float(request.form['winddir9am'])
-		# Wind Dir 3pm
 		winddDir3pm = float(request.form['winddir3pm'])
-		# Wind Gust Dir
 		windGustDir = float(request.form['windgustdir'])
-		# Rain Today
 		rainToday = float(request.form['raintoday'])
 
 		input_lst = [[location, minTemp, maxTemp, rainfall, evaporation, sunshine,
